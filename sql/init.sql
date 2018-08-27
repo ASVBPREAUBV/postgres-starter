132f2769d07e35d4d8a1f57bfa862a5e21ba8659
Paul Mayer <paul@paul-mayer.eu>
2018-08-27 22:44:12 +0200

    init postgrest.conf

--- sql/init.sql
+++ sql/init.sql
@@ -11,15 +11,28 @@ create table api.todos (
 
 /*inserts sample in db*/
 insert into api.todos (task) values
-  ('finish tutorial 0'), ('pat self on back');
+  ('finish tutorial'), ('pat self on back');
 
 /*creates the anonymous role to access the schema (for postgrest)*/
 create role web_anon nologin;
 
 /*give the role access to schema and table*/
 grant usage on schema api to web_anon;
-
 grant all on api.todos to web_anon;
+
+create table api.secret_todos (
+  id serial primary key,
+  done boolean not null default false,
+  task text not null
+);
+
+
+create role secret_todo_user nologin;
+grant secret_todo_user to postgres;
+
+grant usage on schema api to secret_todo_user;
+grant all on api.secret_todos to secret_todo_user;
+/*grant usage, select on sequence api.todos_id_seq to secret_todo_user;*/
 
 /*
 also possible to grant single operations:
@@ -30,3 +43,9 @@ grant DELETE on api.todos to web_anon;
 */
 
 
+/*
+curl http://localhost:3000/secret_todos -X POST \
+-H "Authorization: Bearer $TOKEN"   \
+-H "Content-Type: application/json" \
+-d '{"task": "learn how to auth"}'
+*/
